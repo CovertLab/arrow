185ef386b8c64776357002991da1d225b5741981
Ryan Spangler <spanglry@stanford.edu>
2018-12-13 11:37:24 -0800

    creating a package structure

--- arrow/arrow.py
+++ arrow/arrow.py
@@ -81,35 +81,3 @@ class StochasticSystem(object):
 
 	def evolve(self, state, duration):
 		return evolve(self.reactions, self.weights, state, duration)
-
-
-def johns_system():
-	reactions = np.array([
-		[-1, 1],
-		[1, -1],
-		[0, -1]])
-
-	weights = np.array([10, 10, 0.1])
-
-	state = np.array([1000, 0])
-
-	system = StochasticSystem(reactions, weights)
-	history, steps = system.evolve(state, 1)
-
-	print(zip(steps, history))
-
-
-def test_dimers():
-	reactions = np.array([
-		[-1, -1, 1, 0],
-		[-2, 0, 0, 1],
-		[1, 1, -1, 0]])
-
-	weights = np.array([3, 1, 1])
-
-	state = np.array([1000, 1000, 0, 0])
-
-	system = StochasticSystem(reactions, weights)
-	history, steps = system.evolve(state, 1)
-
-	print(zip(steps, history))
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(
+	name='arrow',
+	version='0.0.1',
+	packages=['arrow'])
