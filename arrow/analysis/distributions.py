45eac040106b6e493d5a6d1508024d6eca1f0c2d
John Mason <jmason42@stanford.edu>
2018-12-19 10:50:00 -0800

    Docstrings for MA stuff

--- arrow/analysis/distributions.py
+++ arrow/analysis/distributions.py
@@ -1,10 +1,58 @@
+'''
+
+This submodule is for statistical reductions on stochastic simulation output.
+
+'''
 
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
 
 def moving_average(time, counts, sampling_time, scale):
-	# TODO: vectorize
+	'''
+	Compute a moving average on stochastic simulation output.
+
+	Parameters
+	----------
+
+	time: 1D ndarray of floats
+		The times at which each timepoint occurs.  Assumed to be strictly
+		increasing.
+
+	counts: 2D ndarray of integers
+		Timepoints should be along the first axis (rows) while individual
+		molecular species should be along the second axis (columns).
+
+	sampling_time: 1D ndarray of floats
+		The new times at which to sample while generating the moving average.
+		Again, assumed to be strictly increasing.
+
+	scale: positive float
+		The time scale of the moving average.  Larger values result in
+		'smoother' curves.
+
+	Returns
+	-------
+
+	averaged: 2D ndarray of floats
+		The moving average of the counts.
+
+	Notes
+	-----
+
+	This function was implemented specifically for stochastic simulation output
+	but is likely applicable to any data.
+
+	As implemented, this moving average will always lag behind the 'true'
+	values.  I.e. an oscillatory trajectory will exhibit a phase shift.
+
+	Too small a scale will appear no different from the unaveraged counts,
+	while too long a scale will lead to extreme lag and wash out any critical
+	magnitude variations.
+
+	This could be sped up considerably if compiled via JIT, or expressed using
+	other compilable operations (e.g. a convolution).
+	'''
 
 	counts = np.asarray(counts)
 
@@ -36,6 +84,11 @@ def moving_average(time, counts, sampling_time, scale):
 	return averaged
 
 def _last_where(bool_array):
+	'''
+	Return the index of the last element that is true in a 1D boolean array.
+
+	This could be made much faster and more robust.
+	'''
 	return np.where(bool_array)[0][-1]
 
 if __name__ == '__main__':
