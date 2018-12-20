4ff80c6fa3efaeb8eb0265b2ffd6eb14d50fc884
Ryan Spangler <spanglry@stanford.edu>
2018-12-19 17:47:45 -0800

    added test for complexation

--- arrow/arrow.py
+++ arrow/arrow.py
@@ -2,91 +2,91 @@ import numpy as np
 
 
 def choose(n, k):
-	terms = [
-		(n + 1.0 - i) / i
-		for i in xrange(1, k + 1)]
-	product = np.array(terms).prod()
-	return np.rint(product)
+    terms = [
+        (n + 1.0 - i) / i
+        for i in xrange(1, k + 1)]
+    product = np.array(terms).prod()
+    return np.rint(product)
 
 
 def propensity(reaction, state, form):
-	reactants = np.where(reaction < 0)
-	terms = [
-		form(state[reactant], -reaction[reactant])
-		for reactant in reactants[0]]
+    reactants = np.where(reaction < 0)
+    terms = [
+        form(state[reactant], -reaction[reactant])
+        for reactant in reactants[0]]
 
-	return np.array(terms).prod()
+    return np.array(terms).prod()
 
 
 def step(reactions, rates, state, forms, propensities=[], update_reactions=()):
-	if len(update_reactions):
-		for update in update_reactions:
-			reaction = reactions[update]
-			form = forms if callable(forms) else forms[update]
-			propensities[update] = propensity(reaction, state, form)
-	else:
-		propensities = [
-			propensity(reaction, state, forms if callable(forms) else forms[index])
-			for index, reaction in enumerate(reactions)]
+    if len(update_reactions):
+        for update in update_reactions:
+            reaction = reactions[update]
+            form = forms if callable(forms) else forms[update]
+            propensities[update] = propensity(reaction, state, form)
+    else:
+        propensities = [
+            propensity(reaction, state, forms if callable(forms) else forms[index])
+            for index, reaction in enumerate(reactions)]
 
-	distribution = (rates * propensities)
-	total = distribution.sum()
-	if total == 0:
-		return (state, 0, -1, propensities)
+    distribution = (rates * propensities)
+    total = distribution.sum()
+    if total == 0:
+        return (state, 0, -1, propensities)
 
-	dt = np.random.exponential(1 / total)
-	random = np.random.uniform(0, 1) * total
-		
-	progress = 0
-	for choice, interval in enumerate(distribution):
-		progress += interval
-		if random <= progress:
-			break
+    dt = np.random.exponential(1 / total)
+    random = np.random.uniform(0, 1) * total
+        
+    progress = 0
+    for choice, interval in enumerate(distribution):
+        progress += interval
+        if random <= progress:
+            break
 
-	reaction = reactions[choice]
-	outcome = state + reaction
+    reaction = reactions[choice]
+    outcome = state + reaction
 
-	return outcome, dt, choice, propensities
+    return outcome, dt, choice, propensities
 
 
 def evolve(reactions, rates, state, duration, forms=choose):
-	time = 0
-	history = [state]
-	steps = [0]
-	propensities = []
-	update_reactions = []
+    time = 0
+    history = [state]
+    steps = [0]
+    propensities = []
+    update_reactions = []
 
-	while True:
-		state, dt, choice, propensities = step(
-			reactions,
-			rates,
-			state,
-			forms,
-			propensities,
-			update_reactions)
+    while True:
+        state, dt, choice, propensities = step(
+            reactions,
+            rates,
+            state,
+            forms,
+            propensities,
+            update_reactions)
 
-		time += dt
-		if time > duration:
-			break
+        time += dt
+        if time > duration:
+            break
 
-		history.append(state)
-		steps.append(time)
+        history.append(state)
+        steps.append(time)
 
-		reaction = reactions[choice]
-		involved = np.where(reaction != 0)
-		update_reactions = np.where(reactions[:, involved] != 0)[0]
+        reaction = reactions[choice]
+        involved = np.where(reaction != 0)
+        update_reactions = np.where(reactions[:, involved] != 0)[0]
 
-	return history, steps
+    return history, steps
 
 
 class StochasticSystem(object):
-	def __init__(self, reactions, rates, forms=None):
-		self.reactions = reactions
-		self.rates = rates
-		self.forms = forms or choose
+    def __init__(self, reactions, rates, forms=None):
+        self.reactions = reactions
+        self.rates = rates
+        self.forms = forms or choose
 
-	def step(self, state):
-		return step(self.reactions, self.rates, state, forms=self.forms)
+    def step(self, state):
+        return step(self.reactions, self.rates, state, forms=self.forms)
 
-	def evolve(self, state, duration):
-		return evolve(self.reactions, self.rates, state, duration, forms=self.forms)
+    def evolve(self, state, duration):
+        return evolve(self.reactions, self.rates, state, duration, forms=self.forms)
--- arrow/test/test_arrow.py
+++ arrow/test/test_arrow.py
@@ -1,77 +1,109 @@
 import numpy as np
+import json
 
 from arrow import evolve, StochasticSystem
 
-
 def johns_system():
-	reactions = np.array([
-		[-1, 1],
-		[1, -1],
-		[0, -1]])
+    reactions = np.array([
+        [-1, 1],
+        [1, -1],
+        [0, -1]])
 
-	rates = np.array([10, 10, 0.1])
-	system = StochasticSystem(reactions, rates)
+    rates = np.array([10, 10, 0.1])
+    system = StochasticSystem(reactions, rates)
 
-	state = np.array([1000, 0])
-	duration = 1
+    state = np.array([1000, 0])
+    duration = 1
 
-	history, steps = system.evolve(state, duration)
+    history, steps = system.evolve(state, duration)
 
-	assert history[-1].sum() < state.sum()
-	assert steps[-1] <= duration
+    assert history[-1].sum() < state.sum()
+    assert steps[-1] <= duration
 
-	return (history, steps)
+    return (history, steps)
 
 
 def test_dimers():
-	reactions = np.array([
-		[-1, -1, 1, 0],
-		[-2, 0, 0, 1],
-		[1, 1, -1, 0]])
+    reactions = np.array([
+        [-1, -1, 1, 0],
+        [-2, 0, 0, 1],
+        [1, 1, -1, 0]])
 
-	rates = np.array([3, 1, 1])
-	system = StochasticSystem(reactions, rates)
+    rates = np.array([3, 1, 1]) * 0.01
+    system = StochasticSystem(reactions, rates)
 
-	state = np.array([1000, 1000, 0, 0])
-	duration = 1
+    state = np.array([1000, 1000, 0, 0])
+    duration = 1
 
-	history, steps = system.evolve(state, duration)
+    history, steps = system.evolve(state, duration)
 
-	assert steps[-1] <= duration
+    assert steps[-1] <= duration
 
-	return (history, steps)
+    return (history, steps)
 
+
+def test_complexation():
+    with open('data/complexation/complexation-state.json', 'r') as file:
+        data = json.load(file)
+
+    assert len(data) == 3
+
+    stoichiometry = np.array(data['stoichiometry']).transpose()
+    state = np.array(data['before'])
+    expected = np.array(data['after'])
+    duration = 1
+
+    assert len(state) == len(expected)
+    assert stoichiometry.shape[1] == len(state)
+
+    # semi-quantitative rate constants
+    rates = np.full(stoichiometry.shape[0], 3)
+
+    system = StochasticSystem(stoichiometry, rates)
+
+    history, steps = system.evolve(state, duration)
+
+    outcome = history[-1]
+    difference = (expected - outcome)
+    total = np.abs(difference).sum()
+
+    print('differences: {}'.format(total))
+    print('total steps: {}'.format(len(steps)))
+    print(steps)
+
+    return (history, steps)
+    
 
 if __name__ == '__main__':
-	from itertools import izip
+    from itertools import izip
 
-	import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-	from arrow.plotting import plot_full_history
+    from arrow.plotting import plot_full_history
 
-	systems = (johns_system, test_dimers)
+    systems = (johns_system, test_dimers, test_complexation, test_complexation)
 
-	n_systems = len(systems)
+    n_systems = len(systems)
 
-	ncols = int(np.ceil(np.sqrt(n_systems)))
-	nrows = int(np.ceil(n_systems / ncols))
+    ncols = int(np.ceil(np.sqrt(n_systems)))
+    nrows = int(np.ceil(n_systems / ncols))
 
-	margins = 1
-	axes_size = 3
+    margins = 1
+    axes_size = 3
 
-	figsize = (
-		margins + axes_size*ncols,
-		margins + axes_size*nrows
-		)
+    figsize = (
+        margins + axes_size*ncols,
+        margins + axes_size*nrows
+        )
 
-	(fix, all_axes) = plt.subplots(
-		figsize = figsize,
-		nrows = nrows, ncols = ncols,
-		constrained_layout = True
-		)
+    (fix, all_axes) = plt.subplots(
+        figsize = figsize,
+        nrows = nrows, ncols = ncols,
+        constrained_layout = True
+        )
 
-	for (axes, system) in izip(all_axes, systems):
-		axes.set_title(system.func_name)
-		plot_full_history(axes, *system()[::-1])
+    for (axes, system) in izip(all_axes.flatten(), systems):
+        axes.set_title(system.func_name)
+        plot_full_history(axes, *system()[::-1])
 
-	plt.savefig('test_systems.png')
+    plt.savefig('test_systems.png')
