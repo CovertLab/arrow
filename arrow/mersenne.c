248239356518a8ea0614fefe5bb68fda11e79524
Ryan Spangler <ryan.spangler@gmail.com>
2019-02-01 16:59:36 -0800

    macro hygiene

--- arrow/mersenne.c
+++ arrow/mersenne.c
@@ -25,7 +25,7 @@
  * have a seed value of 624*32-31 = 19937 bits.
  */
 #define PERIOD 397
-#define DIFF (TWISTER_SIZE - PERIOD)
+#define DIFF ((TWISTER_SIZE) - (PERIOD))
 
 static const uint32_t MAGIC = 0x9908b0df;
 
