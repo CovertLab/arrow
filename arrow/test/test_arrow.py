e9616d75186fa46a11fc26b5ccf122fd6ea2fcfd
cs <cs.temporary@icloud.com>
2021-10-26 18:59:00 +0000

    Pickle RNG state

--- arrow/test/test_arrow.py
+++ arrow/test/test_arrow.py
@@ -273,6 +273,31 @@ def test_flagella():
 
     print('flagella result: {}'.format(result))
 
+def test_get_set_random_state():
+    stoich = np.array([[1, 1, -1, 0], [-2, 0, 0, 1], [-1, -1, 1, 0]])
+    system = StochasticSystem(stoich)
+
+    rand_state = system.obsidian.get_random_state()
+
+    state = np.array([1000, 1000, 0, 0])
+    rates = np.array([3.0, 1.0, 1.0])
+
+    result_1 = system.evolve(1, state, rates)
+    result_2 = system.evolve(1, state, rates)
+
+    differ = False
+    for key in ('time', 'events', 'occurrences', 'outcome'):
+        if np.any(result_1[key] != result_2[key]):
+            differ=True
+
+    assert differ
+
+    system.obsidian.set_random_state(*rand_state)
+    result_2 = system.evolve(1, state, rates)
+
+    for key in ('time', 'events', 'occurrences', 'outcome'):
+        np.testing.assert_array_equal(result_1[key], result_2[key])
+
 
 def main(args):
     systems = (
